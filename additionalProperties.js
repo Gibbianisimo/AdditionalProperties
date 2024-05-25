142a3543b4faa8c6e523ab39aac190a782ca74b9
Gibbianisimo <gibbian@outlook.com>
2024-05-25 16:50:09 -0400

    Add files via upload

---
+++ additionalProperties.js
@@ -0,0 +1,15 @@
+Hooks.on('ready', () => {
+CONFIG.DND5E.weaponProperties['close-quarter'] = 'Close-quarter';
+CONFIG.DND5E.weaponProperties['flex'] = 'Flex';
+CONFIG.DND5E.weaponProperties['graze'] = 'Graze';
+CONFIG.DND5E.weaponProperties['impact'] = 'Impact';
+CONFIG.DND5E.weaponProperties['nick'] = 'Nick';
+CONFIG.DND5E.weaponProperties['polish'] = 'Polish';
+CONFIG.DND5E.weaponProperties['punch'] = 'Punch';
+CONFIG.DND5E.weaponProperties['push'] = 'Push';
+CONFIG.DND5E.weaponProperties['sap'] = 'Sap';
+CONFIG.DND5E.weaponProperties['slow'] = 'Slow';
+CONFIG.DND5E.weaponProperties['sweep'] = 'Sweep';
+CONFIG.DND5E.weaponProperties['topple'] = 'Topple';
+CONFIG.DND5E.weaponProperties['vex'] = 'Vex';
+});
